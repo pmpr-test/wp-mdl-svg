79db4fcfc696cf846814a3033a061e63b52aca33
pmpr.dev <pmpr.dev@gmail.com>
2025-04-26 22:07:17 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             680d57804afdf             |
+    |             680d58ee05c58             |
     |_______________________________________|
 */
  use Pmpr\Module\SVG\SVG; SVG::symcgieuakksimmu();
--- src/Pmpr/Module/SVG/Sanitizer/Tag.php
+++ src/Pmpr/Module/SVG/Sanitizer/Tag.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             680d57804afdf             |
+    |             680d58ee05c58             |
     |_______________________________________|
 */
  namespace Pmpr\Module\SVG\Sanitizer; use enshrined\svgSanitize\data\AllowedTags; use Pmpr\Common\Foundation\Functions\Traits\HelperTrait; class Tag extends AllowedTags { use HelperTrait; public static function getTags() { $kmmywmgcgwceeqii = parent::getTags(); return self::iwgqamekocwaigci()->mmsykuomogaqoaye()->ocksiywmkyaqseou('svg_allowed_tags', $kmmywmgcgwceeqii); } }
